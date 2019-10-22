41f336176803f37a0d926ea516f0ff3c9b28d26e
Zoleka <zrnkubheka@gmail.com>
2019-10-22 10:42:12 +0200

    6th Commit

--- Password_Checker/spec/passwordSpec.js
+++ Password_Checker/spec/passwordSpec.js
@@ -1,34 +1,39 @@
 
-describe('is  password valid', function () {
-    it('should see if password contains only lowercase', function () {
+describe('is  password valid', () => {
+    it('should see if password contains only lowercase', () => {
         let validPassword  = password_is_valid('PASSWORD');
         expect(validPassword ).toBe(false);
     });
 
-    it('should see if password contains uppercase', function () {
+    it('should see if password contains uppercase', () => {
         let validPassword  = password_is_valid('password');
         expect(validPassword ).toBe(false);
     });
 
-    it('should see if password contains numbers', function () {
+    it('should see if password contains numbers', () => {
         let validPassword = password_is_valid('Password123');
         expect(validPassword).toBeTruthy;
     });
 
-    it('should see if password is longer than 8 characters', function () {
+    it('should see if password is longer than 8 characters', () => {
         let validPassword  = password_is_valid('Password123');
         expect(validPassword ).toBeTruthy;
     });
 
-    it('should see if password contains at least 1 lowercase, 1 uppercase, 1 number and has to be longer than 8 characters', function () {
+    it('should see if password contains at least 1 lowercase, 1 uppercase, 1 number and has to be longer than 8 characters', () => {
         let validPassword  = password_is_valid('Password012');
-        expect(validPassword ).toBeTruthy
+        expect(validPassword ).toBeTruthy;
         ;
     });
+    it('should check if the password is vaild and throw an error if it is not valid', () => {
+        let validPassword = password_is_valid('password');
+        expect(validPassword).toThrow(new Error('Password is incorrect'));
+    });
+
 });
 
-describe('is password ok', function () {
-    it('should not be void, must have more than 8 characters, have atleast 1 number and 1 lowercase and 1 uppercase', function () {
+describe('is password ok', () => {
+    it('should not be void, must have more than 8 characters, have atleast 1 number and 1 lowercase and 1 uppercase', () => {
         let passwordChecker  = password_is_ok('Password012');
         expect(passwordChecker).toBeTruthy;
     });
